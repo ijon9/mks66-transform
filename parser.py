1bb611e1a1ec8cc5d17b4b1e0850b4430295f66c
unknown <ijon@stuy.edu>
2019-03-04 22:20:13 -0500

    Some progress

--- parser.py
+++ parser.py
@@ -37,7 +37,50 @@ def parse_file( fname, points, transform, screen, color ):
     print(f)
     for i in range(0, len(f)):
         if f[i] == "line":
-        if f[i] == "line":
+            print("line")
+            pts = f[i+1].split(" ")
+            for x in range(0, len(pts)):
+                pts[x] = int(pts[x])
+            add_edge(points, pts[0], pts[1], pts[2], pts[3], pts[4], pts[5])
+        elif f[i] == "ident":
+            print("ident")
+            ident(transform)
+        elif f[i] == "scale":
+            print("scale")
+            pts = f[i+1].split(" ")
+            matrix_mult(make_scale(pts[0], pts[1], pts[2]), transform)
+        elif f[i] == "translate":
+            print("translate")
+            pts = f[i+1].split(" ")
+            matrix_mult(make_translate(pts[0], pts[1], pts[2]), transform)
+        elif f[i] == "rotate":
+            print("rotate")
+            pts = f[i+1].split(" ")
+            if pts[0] == "x":
+                matrix_mult(make_rotX(pts[1]), transform)
+            elif pts[0] == "y":
+                matrix_mult(make_rotY(pts[1]), transform)
+            elif pts[0] == "z":
+                matrix_mult(make_rotZ(pts[1]), transform)
+        elif f[i] == "apply":
+            print("apply")
+            matrix_mult(transform, points)
+        elif f[i] == "display":
+            print("display")
+            clear_screen(screen)
+            draw_lines(points, screen, color)
+            display(screen)
+        elif f[i] == "save":
+            print("save")
+            clear_screen(screen)
+            draw_lines(points, screen, color)
+            save_extension(f[i+1])
+        elif f[i] == "quit":
+            return
 
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
 
-parse_file("script",2,3,4,5)
+parse_file( 'script', edges, transform, screen, color )
